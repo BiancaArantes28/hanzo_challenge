19b3cb0db3cdec8c9566f21ff788104119ff3eda
BiancaArantes28 <biancaarantes28@gmail.com>
2020-03-25 20:02:00 -0300

    Create actions to get post details

--- src/store/actions/posts/postsActions.js
+++ src/store/actions/posts/postsActions.js
@@ -6,6 +6,10 @@ export const DELETE_POSTS = 'DELETE_POSTS';
 export const DELETE_POSTS_SUCCESSFUL = 'DELETE_POSTS_SUCCESSFUL';
 export const DELETE_POSTS_FAILED = 'DELETE_POSTS_FAILED';
 
+export const POST_DETAILS = 'POST_DETAILS';
+export const POST_DETAILS_SUCCESSFUL = 'POST_DETAILS_SUCCESSFUL';
+export const POST_DETAILS_FAILED = 'POST_DETAILS_FAILED';
+
 export const fetchPosts = (payload = "") => ({
     type: FETCH_POSTS,
     payload,
@@ -33,5 +37,20 @@ export const deletePostsSuccessful = (payload) => ({
 
 export const deletePostsFailed = (error) => ({
     type: DELETE_POSTS_FAILED,
+    payload: error,
+});
+
+export const postDetails = (payload) => ({
+    type: POST_DETAILS,
+    payload,
+});
+
+export const postDetailsSuccessful = (payload) =>  ({
+    type: POST_DETAILS_SUCCESSFUL,
+    payload,
+});
+
+export const postDetailsFailed = (error) => ({
+    type: POST_DETAILS_FAILED,
     payload: error,
 });
