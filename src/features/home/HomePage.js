fe2eb1450d722844e85025ad8235f3953e5f4832
BiancaArantes28 <biancaarantes28@gmail.com>
2020-03-25 16:31:38 -0300

    Render cards with posts

--- src/features/home/HomePage.js
+++ src/features/home/HomePage.js
@@ -1,46 +1,90 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Spinner from '../../common/spinner/Spinner';
 import { HELLOWORLD_STATUS } from '../../store/reducers/helloWorldReducer';
+import { POSTS_STATUS } from '../../store/reducers/posts/postsReducers';
 
+import CardComponent from '../../common/card/CardComponent';
+import DepositPhotos from '../../assets/img/img_depositphotos.jpg'
 import './home.css';
+
+const styles = theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    subtitle: {
+        color: theme.palette.text.secondary,
+    }
+});
 
 class HomePage extends Component {
 
     renderLoading() {
-        return <Spinner />;
+        return (
+            <Grid item md={12} xs={12}>
+                <Spinner />
+            </Grid>
+        );
     }
 
-    renderBody() {
+    renderTitle(title, styledClass) {
         return (
-            <span>{this.props.helloWorld}</span>
+            <div>
+                <Typography variant="h3">
+                    {title}
+                </Typography>
+                <Typography variant="h6" className={styledClass}>
+                    Mar 2020
+                </Typography>
+            </div>
         );
+    }
+
+    renderBody(posts, classes) {
+        let content = <Typography variant="h6">
+            Teste
+        </Typography>
+        return posts.map(m => {
+            return (
+                <Grid item lg={4} md={6} sm={6} xs={12} key={m.id}>
+                    <CardComponent
+                        title={this.renderTitle(m.title, classes.subtitle)}
+                        content={content}
+                        img={DepositPhotos}
+                        titleImg={'Deposit photos'}
+                    />
+                </Grid>
+            );
+        });
     }
 
     render() {
         let content;
-        const { status } = this.props;
+        const { classes, status, posts } = this.props;
 
-        if (status === HELLOWORLD_STATUS.INPROGRESS) {
+        if (status === POSTS_STATUS.INPROGRESS) {
             content = this.renderLoading();
         } else {
-            content = this.renderBody();
+            content = this.renderBody(posts, classes);
         }
 
         return (
-            <div className="home">
-                <Typography variant="h2">
+            <div className={classes.root}>
+                <Grid container spacing={3}>
                     {content}
-                </Typography>
+                </Grid>
+
             </div>
         );
     }
 }
 
 HomePage.propTypes = {
-    helloWorld: PropTypes.string.isRequired,
+    posts: PropTypes.arrayOf(PropTypes.object),
     status: PropTypes.string.isRequired,
 }
 
-export default HomePage;
+export default withStyles(styles, { withTheme: true })(HomePage);
