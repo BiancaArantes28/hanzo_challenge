68424d410d974ef196747807f9231ca46048227f
BiancaArantes28 <biancaarantes28@gmail.com>
2020-03-25 19:35:39 -0300

    Create route to details post

--- src/App.js
+++ src/App.js
@@ -6,26 +6,24 @@ import './App.css';
 import store from './store';
 import Header from './common/header/Header';
 import HomeContainer from './features/home/HomeContainer';
+import DetailsContainer from './features/details/DetailsContainer';
 
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-
         <Router>
-          
           <Container fixed>
             <Header />
             <Switch>
               <Route path="/home" component={HomeContainer} />
+              <Route path="/details/:id" component={DetailsContainer} />
               <Redirect to="/home" />
             </Switch>
           </Container>
         </Router>
-
       </div>
-
     </Provider>
 
   );
