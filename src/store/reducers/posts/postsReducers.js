0074356e65bc553ab70ac3cb82bbbacf727d3f28
BiancaArantes28 <biancaarantes28@gmail.com>
2020-03-25 16:28:13 -0300

    Create reducers posts

---
+++ src/store/reducers/posts/postsReducers.js
@@ -0,0 +1,51 @@
+import _ from 'lodash';
+import {
+    FETCH_POSTS,
+    FETCH_POSTS_SUCCESSFUL,
+    FETCH_POSTS_FAILED,
+} from '../../actions/posts/postsActions';
+
+const withoutError = (state) => _.omit(state, 'error');
+
+export const POSTS_STATUS = {
+    'NOT_FETCHED': 'not fetched',
+    'INPROGRESS': 'inprogress',
+    'FETCHED': 'fetched',
+};
+
+const defaultState = {
+    status: POSTS_STATUS.NOT_FETCHED,
+    posts: [],
+};
+
+const successfulPosts = (state, payload) => {
+    let content;
+
+    if (payload.length > 1) {
+        content = payload;
+    } else {
+        content = [payload];
+    }
+
+    return {
+        ...withoutError(state),
+        status: POSTS_STATUS.FETCHED,
+        posts: content,
+    }
+};
+
+export default function fetchPostsReducer(state = defaultState, action) {
+    switch (action.type) {
+        case FETCH_POSTS:
+            return { ...state, status: POSTS_STATUS.INPROGRESS };
+
+        case FETCH_POSTS_SUCCESSFUL:
+            return successfulPosts(state, action.payload);
+
+        case FETCH_POSTS_FAILED:
+            return { ...state, error: action.payload, status: POSTS_STATUS.FETCHED };
+
+        default:
+            return state;
+    }
+}
