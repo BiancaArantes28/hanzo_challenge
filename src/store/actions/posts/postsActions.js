20876645b2763019a73fdb6060a428f3ddb8d60b
BiancaArantes28 <biancaarantes28@gmail.com>
2020-03-25 19:19:20 -0300

    Create actions to delete post

--- src/store/actions/posts/postsActions.js
+++ src/store/actions/posts/postsActions.js
@@ -2,6 +2,10 @@ export const FETCH_POSTS = 'FETCH_POSTS';
 export const FETCH_POSTS_SUCCESSFUL = 'FETCH_POSTS_SUCCESSFUL';
 export const FETCH_POSTS_FAILED = 'FETCH_POSTS_FAILED';
 
+export const DELETE_POSTS = 'DELETE_POSTS';
+export const DELETE_POSTS_SUCCESSFUL = 'DELETE_POSTS_SUCCESSFUL';
+export const DELETE_POSTS_FAILED = 'DELETE_POSTS_FAILED';
+
 export const fetchPosts = (payload = "") => ({
     type: FETCH_POSTS,
     payload,
@@ -14,5 +18,20 @@ export const fetchPostsSuccessful = (payload) => ({
 
 export const fetchPostsFailed = (error) => ({
     type: FETCH_POSTS_FAILED,
+    payload: error,
+});
+
+export const deletePosts = (payload) => ({
+    type: DELETE_POSTS,
+    payload,
+});
+
+export const deletePostsSuccessful = (payload) => ({
+    type: DELETE_POSTS_SUCCESSFUL,
+    payload,
+});
+
+export const deletePostsFailed = (error) => ({
+    type: DELETE_POSTS_FAILED,
     payload: error,
 });
