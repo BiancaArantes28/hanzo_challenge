20b457c89b8346eecc85dd0e7ab2a731cc986e1c
BiancaArantes28 <biancaarantes28@gmail.com>
2020-03-25 19:20:01 -0300

    Create saga to delete post

--- src/store/sagas/posts/postsSagas.js
+++ src/store/sagas/posts/postsSagas.js
@@ -1,9 +1,13 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects';
+import { all, call, put, select, takeLatest } from 'redux-saga/effects';
 import {
     FETCH_POSTS,
     fetchPostsSuccessful,
     fetchPostsFailed,
+    DELETE_POSTS,
+    deletePostsSuccessful,
+    deletePostsFailed,
 } from '../../actions/posts/postsActions';
+import { getPosts } from '../../selectors/posts/postsSelectors';
 import { getAPIURL } from '../../../config/getPATH';
 import { fetchGet } from '../sagaUtils';
 
@@ -29,8 +33,21 @@ export function* doFetchPosts(action) {
     }
 }
 
+export function* doDeletePosts(action) {
+    try {
+        let posts = yield select(getPosts);
+
+        let result = posts.filter(p => p.id !== action.payload);
+
+        yield put(deletePostsSuccessful(result));
+    } catch (error) {
+        yield put(deletePostsFailed(error));
+    }
+}
+
 export default function* rootSaga() {
     return yield all([
-        takeLatest(FETCH_POSTS, doFetchPosts)
+        takeLatest(FETCH_POSTS, doFetchPosts),
+        takeLatest(DELETE_POSTS, doDeletePosts)
     ])
 }
